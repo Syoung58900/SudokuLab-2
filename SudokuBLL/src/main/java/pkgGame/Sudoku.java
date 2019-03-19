91c4bef7866f0552b8522f496dc391cb068a89a3
kimberly <kimberly@10.119.48.120>
2019-03-18 20:29:00 -0400

    pkgGame v2

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -6,62 +6,97 @@ public class Sudoku extends LatinSquare {
 	int iSize;
 	int iSqrtSize;
 
-	public Sudoku() {
+	public Sudoku(int iSize) throws Exception {
 		super();
-
+		this.iSize = iSize;
+		if (Math.sqrt(iSize) == (int) Math.sqrt(iSize))
+			this.iSqrtSize = (int) Math.sqrt(iSize);
+		else
+			throw new Exception("Size does not have integer square root");
 	}
 
-	public Sudoku(int[][] puzzle) {
+	public Sudoku(int[][] puzzle) throws Exception {
 		super(puzzle);
+
+		iSize = puzzle.length;
+
+		if (Math.sqrt(iSize) == (int) Math.sqrt(iSize))
+			this.iSqrtSize = (int) Math.sqrt(iSize);
+		else
+			throw new Exception("Size does not have integer square root");
 
 	}
 
 	public int[][] getPuzzle() {
-		return null;
+		return super.getLatinSquare();
 	}
-	
-	public int[] getRegion(int iRow, int iCol) throws Exception {
-		int[][] puzzle = this.getPuzzle();
-		if (iCol >=iSize || iRow >= iSize) {
-			throw new Exception("Region Index Out of Bounds");
-		}
-		else {
-			int iSize = puzzle.length;
-			int iSizeSqrt = (int) Math.sqrt(iSize);
-			int i = (iCol/iSizeSqrt) + ((iRow/iSizeSqrt)*iSizeSqrt);
-			return getRegion(i);
-		}
+
+	public int[] getRegion(int iRow, int iCol) {
+		int i = (iCol / iSqrtSize) + ((iRow / iSqrtSize) * iSqrtSize);
+		return getRegion(i);
 	}
-	
-	protected int[] getRegion(int iReg) throws Exception {
+
+	public int[] getRegion(int iReg) {
 		int[][] puzzle = this.getPuzzle();
 		int[] region = new int[puzzle.length];
 		int index = 0;
-		int iSize = puzzle.length;
-		int iSizeSqrt = (int) Math.sqrt(iSize);
-		if(iReg >= iSize)
-			throw new Exception ("Region Index Out of Bounds");
-		for (int iRow = (iReg/iSizeSqrt)* iSizeSqrt; iRow < (((iReg/iSize) +1)*iSizeSqrt); iRow ++){
-			for(int iCol = (iReg % iSizeSqrt)* iSizeSqrt; iCol < (((iReg % iSizeSqrt)+1)*iSizeSqrt); iCol++) {
+		for (int iRow = (iReg / iSqrtSize) * iSqrtSize; iRow < (((iReg / iSize) + 1) * iSqrtSize); iRow++) {
+			for (int iCol = (iReg % iSqrtSize) * iSqrtSize; iCol < (((iReg % iSqrtSize) + 1) * iSqrtSize); iCol++) {
 				region[index++] = puzzle[iRow][iCol];
 			}
 		}
 		return region;
 	}
 
-
-
+	public boolean hasDuplicates() {
+		boolean hasDuplicates = false;
+		if (super.hasDuplicates(null)) {
+			return true;
+		}
+		for (int i = 0; i < this.getLatinSquare().length; i++) {
+			if (hasDuplicates(this.getRegion(i))) {
+					hasDuplicates = true;
+			}
+		}
+		return hasDuplicates;
+	}
 
 	public boolean isSoduku() {
-		return false;
+
+		if (!isParitalSoduku())
+			return false;
+
+		if (super.ContainsZero())
+			return false;
+
+		return true;
 	}
 
 	public boolean isParitalSoduku() {
+		for (int i = 0; i < this.iSize; i++) {
+			if (super.hasDuplicates(getRegion(i))) {
+				return false;
+			}
+		}
+
+		if (!super.isLatinSquare()) {
+			return false;
+		}
+
 		return false;
 	}
 
-	public boolean isValidValue(int x, int y, int z) {
-		return false;
+	public boolean isValidValue(int iCol, int iRow, int iValue) {
+		if (doesElementExist(super.getRow(iRow), iValue))
+			return false;
+		else if (doesElementExist(super.getColumn(iCol), iValue))
+			return false;
+		else if (doesElementExist(getRegion(iRow, iCol), iValue))
+			return false;
+
+		return true;
+
 	}
 
 }
+
